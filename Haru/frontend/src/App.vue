64be193a4de8589436cedad0c1c452285e6d074f
Yoengjung <18681002@konyang.ac.kr>
2024-01-15 13:07:59 +0900

    Squashed commit of the following:
    commit 116617069bfa0450aff48cda1ecb5626e49a4e00
    Merge: c4eb67e 3452ddf
    Author: Yoengjung <18681002@konyang.ac.kr>
    Date:   Mon Jan 15 12:46:57 2024 +0900
    
        merge
    
    commit c4eb67e2701a842b63f366d514ae8af348adde87
    Author: Yoengjung <18681002@konyang.ac.kr>
    Date:   Fri Jan 5 17:56:35 2024 +0900
    
        Squashed commit of the following:
    
        commit dba496c94c917228a88873fbbc011eca92355a02
        Author: Yoengjung <18681002@konyang.ac.kr>
        Date:   Fri Jan 5 17:55:01 2024 +0900
    
            Squashed commit of the following:
    
            commit 08a21353851272062ab2661c0da228be992b5865
            Author: IamBS <leebs30919@gmail.com>
            Date:   Fri Jan 5 17:43:51 2024 +0900
    
                frontend 수정
    
                수정
    
            commit 72afae820c5ea880449e9b7ca14b13978c3f73c7
            Author: IamBS <leebs30919@gmail.com>
            Date:   Fri Jan 5 16:46:40 2024 +0900
    
                깃허브 변경 확인
    
            commit 05d608ba8550b024ad9f778375828f00347e6fa8
            Author: IamBS <leebs30919@gmail.com>
            Date:   Fri Jan 5 16:38:15 2024 +0900
    
                0105 디렉터리 변경됨.
    
                0105 디렉터리 변경됨.
    
        commit 2fae6769a6926ae65b39615fc495eda93d609045
        Author: Yoengjung <18681002@konyang.ac.kr>
        Date:   Fri Jan 5 17:45:56 2024 +0900
    
            장고 디렉토리 생성

--- Haru/frontend/src/App.vue
+++ Haru/frontend/src/App.vue
@@ -42,10 +42,73 @@ export default {
 </script>
 
 <style>
+<<<<<<< HEAD
+html {
+  margin: 0px;
+  padding: 0px;
+}
+@font-face {
+  font-family: "SUITE-SemiBold";
+  src: url("./assets/font/SUITE-SemiBold.ttf") format("woff");
+}
+@font-face {
+  font-family: "SUITE-Bold";
+  src: url("./assets/font/SUITE-Bold.ttf") format("woff");
+}
+@font-face {
+  font-family: "SUITE-Heavy";
+  src: url("./assets/font/SUITE-Heavy.ttf") format("woff");
+}
+@font-face {
+  font-family: "SUITE-Light";
+  src: url("./assets/font/SUITE-Light.ttf") format("woff");
+}
+@font-face {
+  font-family: "SUITE-Medium";
+  src: url("./assets/font/SUITE-Medium.ttf") format("woff");
+}
+@font-face {
+  font-family: "SUITE-Regular";
+  src: url("./assets/font/SUITE-Regular.ttf") format("woff");
+}
+@font-face {
+  font-family: "SUITE-ExtraBold";
+  src: url("./assets/font/SUITE-ExtraBold.ttf") format("woff");
+}
+@font-face {
+  font-family: "humanbeomseog";
+  src: url("./assets/font/휴먼범석체.ttf") format("woff");
+}
+
+* {
+  font-family: "SUITE";
+  color: #333333;
+  font-size: 16px;
+  position: relative;
+}
+
+.container {
+  margin: 0 auto;
+}
+
+.container1 {
+  padding: 80px 228px 228px;
+  margin: 0 auto;
+}
+
+a {
+  text-decoration: none;
+}
+
+.cursor-p {
+  cursor: pointer;
+}
+=======
 /* 공통 css 적용 */
 @import url("@/css/client/common/common.css");
 /* 공통 font */
 @import url("@/css/client/common/font.css");
 /* 화면 별 background-image 설정 */
 @import url("@/css/client/common/bgImage.css");
+>>>>>>> 3452ddf317ab63b434bd437bc2e5367c419eef0b
 </style>
